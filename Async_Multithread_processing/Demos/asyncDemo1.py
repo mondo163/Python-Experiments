102c31ad3fd25145308d8c10bf8757ff3125cf3c
mondo163 <59904260+mondo163@users.noreply.github.com>
2022-07-14 11:23:40 -0700

    docs(vscode): edited settings for formatting

--- Async_Multithread_processing/Demos/asyncDemo1.py
+++ Async_Multithread_processing/Demos/asyncDemo1.py
@@ -5,36 +5,48 @@ from tracemalloc import start
 import requests
 from pathlib import Path
 from eliot import start_action, to_file
+
 to_file(open("D:\Repositories\Work-Experiments\Logs\outputAsync.log", "w"))
 
-DOWNLOAD_FOLDER =Path("D:\\Repositories\\Work-Experiments\\Downlaods")
-ISO_FILE="https://cdimage.debian.org/cdimage/archive/9.13.0/amd64/iso-cd/debian-9.13.0-amd64-netinst.iso"
+DOWNLOAD_FOLDER = Path("D:\\Repositories\\Work-Experiments\\Downlaods")
+ISO_FILE = "https://cdimage.debian.org/cdimage/archive/9.13.0/amd64/iso-cd/debian-9.13.0-amd64-netinst.iso"
+
+
 async def GetIsoFile(filename: str):
-    with start_action(action_type=f"Getting {filename}: Started at {time.strftime('%X')}", thread=Thread.__name__) as action:
+    with start_action(
+        action_type=f"Getting {filename}: Started at {time.strftime('%X')}",
+        thread=Thread.__name__,
+    ) as action:
         resp = requests.api.get(ISO_FILE)
         if resp.status_code == 200:
-            open((DOWNLOAD_FOLDER/filename).with_suffix(".iso"), "wb").write(resp.content)
-        action.log(message_type=f"Completed getting {filename} at {time.strftime('%X')}", thread=Thread.__name__)
+            open((DOWNLOAD_FOLDER / filename).with_suffix(".iso"), "wb").write(
+                resp.content
+            )
+        action.log(
+            message_type=f"Completed getting {filename} at {time.strftime('%X')}",
+            thread=Thread.__name__,
+        )
         await asyncio.sleep(1)
 
 
 async def main():
-    """ task = []
+    """task = []
     with start_action(action_type="creating tasks:") as action:
        task1 = asyncio.create_task()
        action.log(message_type="Task 1 created", thread=task1.get_name())
-       task2 = asyncio.create_task(GetIsoFile("file2"))
+       task2 = asyncio.create_task(GetIsoFile("file2"
        action.log(message_type="Task 2 created", thread=task2.get_name())
        task3  = asyncio.create_task(GetIsoFile("file3"))
-       action.log(message_type="Task 2 created", thread=task3.get_name()) """
+       action.log(message_type="Task 2 created", thread=task3.get_name())"""
     tic = time.perf_counter()
-    with start_action(action_type="gathering list of tasks")as action: 
-        await asyncio.gather(GetIsoFile("file1"), GetIsoFile("file2"), GetIsoFile("file3"))
+    with start_action(action_type="gathering list of tasks") as action:
+        await asyncio.gather(
+            GetIsoFile("file1"), GetIsoFile("file2"), GetIsoFile("file3")
+        )
         toc = time.perf_counter()
-    
+
     with start_action(action_type="Performance:") as action:
         action.log(message_type=f"Downloaded the file in {toc -tic:0.4f} seconds")
 
 
-
-asyncio.run(main())    
+asyncio.run(main())
--- Async_Multithread_processing/Demos/multiThreadDemo2.py
+++ Async_Multithread_processing/Demos/multiThreadDemo2.py
@@ -6,33 +6,55 @@ from tracemalloc import start
 import requests
 from pathlib import Path
 from eliot import start_action, to_file, preserve_context
+
 to_file(open("D:\Repositories\Work-Experiments\Logs\outputThread.log", "w"))
 
-DOWNLOAD_FOLDER =Path("D:\\Repositories\\Work-Experiments\\Downlaods")
-ISO_FILE="https://cdimage.debian.org/cdimage/archive/9.13.0/amd64/iso-cd/debian-9.13.0-amd64-netinst.iso"
+DOWNLOAD_FOLDER = Path("D:\\Repositories\\Work-Experiments\\Downlaods")
+ISO_FILE = "https://cdimage.debian.org/cdimage/archive/9.13.0/amd64/iso-cd/debian-9.13.0-amd64-netinst.iso"
+
+
 def GetIsoFile(filename: str):
-    with start_action(action_type=f"Getting {filename}: Started at {time.strftime('%X')}", currentThread=threading.get_ident()) as action:
+    with start_action(
+        action_type=f"Getting {filename}: Started at {time.strftime('%X')}",
+        currentThread=threading.get_ident(),
+    ) as action:
         resp = requests.api.get(ISO_FILE)
         if resp.status_code == 200:
-            open((DOWNLOAD_FOLDER/filename).with_suffix(".iso"), "wb").write(resp.content)
-        action.log(message_type=f"Completed getting {filename} at {time.strftime('%X')}", thread=Thread.__name__)
+            open((DOWNLOAD_FOLDER / filename).with_suffix(".iso"), "wb").write(
+                resp.content
+            )
+        action.log(
+            message_type=f"Completed getting {filename} at {time.strftime('%X')}",
+            thread=Thread.__name__,
+        )
 
 
 def main():
-    """ task = []
+    """task = []
     with start_action(action_type="creating tasks:") as action:
        task1 = asyncio.create_task()
        action.log(message_type="Task 1 created", thread=task1.get_name())
        task2 = asyncio.create_task(GetIsoFile("file2"))
        action.log(message_type="Task 2 created", thread=task2.get_name())
        task3  = asyncio.create_task(GetIsoFile("file3"))
-       action.log(message_type="Task 2 created", thread=task3.get_name()) """
+       action.log(message_type="Task 2 created", thread=task3.get_name())"""
     tic = time.perf_counter()
-    with start_action(action_type="Creating threads and starting threadss")as action: 
-        thread1 = Thread(target=preserve_context(GetIsoFile),kwargs={"filename":"iso1"})
-        thread2 = Thread(target=preserve_context(GetIsoFile),kwargs={"filename":"iso2"})
-        thread3 = Thread(target=preserve_context(GetIsoFile),kwargs={"filename":"iso3"})
-        action.log(message_type="Thread ids:", thread1=thread1.name, thread2=thread2.name, thread3=thread3.name)
+    with start_action(action_type="Creating threads and starting threadss") as action:
+        thread1 = Thread(
+            target=preserve_context(GetIsoFile), kwargs={"filename": "iso1"}
+        )
+        thread2 = Thread(
+            target=preserve_context(GetIsoFile), kwargs={"filename": "iso2"}
+        )
+        thread3 = Thread(
+            target=preserve_context(GetIsoFile), kwargs={"filename": "iso3"}
+        )
+        action.log(
+            message_type="Thread ids:",
+            thread1=thread1.name,
+            thread2=thread2.name,
+            thread3=thread3.name,
+        )
         thread1.start()
         thread2.start()
         thread3.start()
@@ -41,7 +63,7 @@ def main():
         thread2.join()
         thread3.join()
         toc = time.perf_counter()
-    
+
     with start_action(action_type="Performance:") as action:
         action.log(message_type=f"Downloaded the files in {toc -tic:0.4f} seconds")
 
